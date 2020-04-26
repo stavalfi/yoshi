76b71f7c4caa1fc08e029fe80ab6b876ea7b2c14
stavalfi <stavalfi@gmail.com>
2020-04-26 16:58:25 +0300

    wip

--- packages/create-yoshi-app/templates/out-of-iframe/javascript/src/viewerApp/viewerApp.e2e.js
+++ packages/create-yoshi-app/templates/out-of-iframe/javascript/src/viewerApp/viewerApp.e2e.js
@@ -4,7 +4,7 @@ describe('Viewer App', () => {
   it('should display the title text', async () => {
     await page.goto(viewerUrl);
     await page.waitForSelector('h2', {
-      timeout: 15000,
+      timeout: 15000 * 2,
     });
 
     expect(await page.$eval('h2', e => e.textContent)).toEqual('Hello World!');
